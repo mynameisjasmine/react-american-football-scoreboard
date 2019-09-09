21f3ea2ac1e4ab3695bc92a4739c5ba2f027e14c
Jasmine <jasminecharles@hotmail.com>
2019-09-09 12:38:06 -0700

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BottomRow from "./BottomRow";
 import React, { useState } from "react";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [countLions, setCountLions] = useState(0);
   const [countTigers, setCountTigers] = useState(0);
   return (
